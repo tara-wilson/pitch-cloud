188d060f736d5bde7c1d222f599e0c98c4780409
Tara Wilson <1tarawilson+gh@gmail.com>
2021-02-12 15:47:03 -0500

    clean

--- functions/config.js
+++ functions/config.js
@@ -1,6 +1,6 @@
 module.exports = {
   stripeSecretKey:
     "sk_test_51HladEDrxasR3GjSIXMorenTurJJXABQTb1Zw6N5ShEuSrNOOMO85nfOxsdbZ1zKPBjV1xr9GuSUtPL9pHxsg3Y400juJcCzxf",
-  customersCollectionPath: "stripe-customers",
+  customersCollectionPath: "stripeCustomers",
   syncUsersOnCreate: true,
 };
